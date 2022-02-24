1fa12d7933793cda178ff536dde769cd0c33e16c
안효인 <kjr04205@nate.com>
2022-02-24 22:27:43 +0900

    spring web version to update 5.0.7

--- src/main/java/com/project/ManagementProgram/LoginController.java
+++ src/main/java/com/project/ManagementProgram/LoginController.java
@@ -1,12 +1,17 @@
 package com.project.ManagementProgram;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/login")
 public class LoginController {
-	@RequestMapping("/login")
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
